1ba8d68d808ea8d639c886b0de67eaa3af25f307
Sigma117 <71655239+Sigma117@users.noreply.github.com>
2022-05-11 22:56:03 +0200

    Update take_13,20.py

--- take_13,20.py
+++ take_13,20.py
@@ -2,8 +2,7 @@ import os
 import numpy as np
 import glob
 
-# per creare una liste di tutte le cartelle
-path = "/Users/stefanozhao/Desktop/dataset/Duck" #pwd sul terminale
+path = "/Users/stefanozhao/Desktop/dataset/Duck"
 directory_contents = os.listdir(path)
 all_subDirectory = list
 
@@ -12,13 +11,15 @@ for item in directory_contents:
         directory_contents.remove(item)
     if item == "labels":
         directory_contents.remove(
-            "labels")  # per rimuore la cartella labels da quelle in qui dobbiamo estrarrre le immagini
+            "labels")  
 
 
 all_subDirectory = directory_contents
 all_subDirectory.sort()
 print(all_subDirectory)
-"""copia e estrazione """
+
+"""copy and extraction """
+
 for i in all_subDirectory:
     bashCommand_mv1 = ("cp /Users/stefanozhao/Desktop/dataset/Duck/{}/{}_13.jpg /Users/stefanozhao/Desktop/dataset/Duck/{}/{}_20.jpg /Users/stefanozhao/Desktop/dataset/Duck/labels".format(i, i, i, i))
     os.system(bashCommand_mv1)
