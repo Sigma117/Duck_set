e6f25f60a10de65f22d0e6042b352bb3dbd15691
Sigma117 <71655239+Sigma117@users.noreply.github.com>
2022-05-11 22:44:22 +0200

    Update chaging_Video_Resoluzion.py

--- chaging_Video_Resoluzion.py
+++ chaging_Video_Resoluzion.py
@@ -3,14 +3,12 @@ import cv2
 import glob
 import os
 
-#script che permette di confertire i video in 256 x 128
-# da un errore che non compredo
 
 def video_resize(input_path, output_path):
     cap = cv2.VideoCapture(input_path)
 
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter(output_path, fourcc, 30.0, (256, 128))  # se ci metti output.mp4v non sa errore ma non esce l'output
+    out = cv2.VideoWriter(output_path, fourcc, 30.0, (256, 128))
 
     while True:
         ret, frame = cap.read()
@@ -26,18 +24,11 @@ def video_resize(input_path, output_path):
     cv2.destroyAllWindows()
 
 
-#video_resize("20160408151056_ayrton.video.mp4","1.mp4")
-
 all_video = [i for i in glob.glob("*.mp4")]
 j = 1
 for i in all_video:
     output = str(j) + ".mp4"
-    video_resize(i, output)  #[:-4] dalla fine -4 lettere
+    video_resize(i, output)
     j = j+1
 
 print("done")
-
-
-
-#print (all_video)
-#print (len(all_video))
